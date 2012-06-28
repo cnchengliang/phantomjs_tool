18ce689076b4a02351068a213e439fd11e6cc9f2
Cheng Liang <233_clcl@163.com>
2012-06-29 06:39:53 +0800

    vote

--- phantomjs_tools_web/js/views/test/vote.js
+++ phantomjs_tools_web/js/views/test/vote.js
@@ -2,9 +2,9 @@ define([
 	'jQuery', 
 	'Underscore', 
 	'Backbone',
-	'text!templates/test.html',
+	'text!templates/vote.html',
 	'libs/function.common'
-	], function ($, _, Backbone, testTemplate) {
+	], function ($, _, Backbone, voteTemplate) {
 
     var view = Backbone.View.extend({
         el: $("#page"),
@@ -14,24 +14,31 @@ define([
 		    page:1
         },
         initialize: function () {
-        	//$('body').append(testTemplate);
+        	//$('body').append(voteTemplate);
         },
         setOptions: function (cfg) {
         	this.opts = $.extend({}, this.opts, cfg);
         },
         startVote: function()
 		{
-			_this = this;
+        	//curl --data "ID=1963&page=10&yname=&lie2=小红帽长沙星城国际幼儿园&file1=xiaolianbb_ll7&name1=" http://www.zgxhm.com/xiaolianbb_taobiao.asp
+			$('form[name=form1963]').submit();
+			//alert($("form[name=form1963]").attr("ID"));
+			alert($('body').text());
+			alert('timeout:>');
+			
+        	/*_this = this;
 			$.ajax({
 				url: _this.opts.url,
 				type: 'POST',
-				//data: $("#loginForm",document.getElementById('main').contentDocument).serialize(),
+				data: 'ID=1963&page=10&yname=&lie2=小红帽长沙星城国际幼儿园&file1=xiaolianbb_ll7&name1=',//$("#loginForm",document.getElementById('main').contentDocument).serialize(),
 				async: true,
 				dataType: 'html',
-				timeout: 3000,
-				error:function()
+				timeout: 30000,
+				error:function(jqXHR, textStatus, errorThrown)
 				{
-					alert('timeout:>'+_this.opts.page);
+					alert(JSON.stringify(jqXHR),textStatus, errorThrown);
+					alert('t2imeout:>'+_this.opts.url);
 					setTimeout(function(){_this.startVote();}, 1000);
 				}
 				,
@@ -42,7 +49,7 @@ define([
 						setTimeout(function(){_this.startVote();}, 1000);
 					}
 				}
-			});
+			});*/
 		},
 		f5: function()
 		{
